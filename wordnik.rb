a67505fbe3a0fb3f8bec113db3316ddc4d2e27de
brian <riceb53@gmail.com>
2019-10-31 20:47:04 -0500

    show definition with user input

--- wordnik.rb
+++ wordnik.rb
@@ -1,5 +1,19 @@
 require 'http'
+require 'awesome_print'
+# Create a new ruby file called dictionary_app.rb
+# The program should ask the user to enter a word, then use the wordnik API to show the word’s definition: https://developer.wordnik.com/docs#!/word/getDefinitions
+# Once that works, have it also display the top example and pronunciation (browse through the documentation to the top example and pronunciation).
+# Bonus: Write your program in a loop such that the user can keep entering new words without having to restart the program. Give them the option of entering q to quit.
 
-response = HTTP.get("https://api.wordnik.com/v4/word.json/Halloween/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
+# use wordnik api to find the definition
+# user enters a word
+# use wordnik to display top example
+# use wordnik to display pronunciation
+# make a loop so user can continue entering words
 
-p response.parse
+user_word = gets.chomp
+
+response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
+
+ap response.parse[0]['text']
+text = response.parse[0]['text']
